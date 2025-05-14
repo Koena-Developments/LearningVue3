e84cd71c9d87a6e97174778b16ff98b07eb73956
Thabang Motswenyane <thabangmotswenyane511@gmail.com>
2025-05-14 11:39:06 +0200

    implemented consistent styles for the text and fixed the card sizing and changed the overflow of the text

--- e-commerce/src/components/ProductCard.vue
+++ e-commerce/src/components/ProductCard.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="item">
     <img :src="product.image" :alt="product.title" />
+    
+    <div class="details">
+    
+    <div class="title">
     <h2>{{ product.title }}</h2>
-    <div class="price">R{{ product.price.toFixed(2) }}</div>
-    <button @click="$emit('add-to-cart', product)">Add to Cart</button>
-    <button @click="$emit('remove-product', product)">Remove</button>
+    </div>
+    
+    <div class="price">R{{ product.price.toFixed(2) }}
+
+    </div>
+    
+    <div>
+      <button @click="$emit('add-to-cart', product)">Add to Cart</button>
+     <button @click="$emit('remove-product', product)">Remove</button> 
+    </div>
+    
+    <div>
     <router-link :to="`/product/${product.id}`"><button>View Product</button></router-link>
+    </div>  
+  
+  </div>
+  
   </div>
 </template>
 
@@ -24,15 +41,47 @@ export default {
 <style scoped>
 
 .item {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background: #EEEEE6;
   padding: 20px;
   border-radius: 20px;
+  filter: drop-shadow(0 50px 20px #0009);
+  width: 200px;
+  height: auto;
+  cursor: pointer;
 }
 
-.item img {
+
+.details
+{
+  position: absolute;
   width: 90%;
-  filter: drop-shadow(0 50px 20px #0009);
+  height: auto;
+  border: 1px solid red;
+  padding: 15px;
 }
+
+
+.item .title
+{
+  font-size: 8px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  height: 86px;
+}
+.item img {
+  border-radius: 4px solid red;
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+}
+/* .item img:hover {
+  zoom:50%;
+} */
 
 .item h2 {
   font-weight: 500;
@@ -41,7 +90,7 @@ export default {
 
 .item .price {
   letter-spacing: 7px;
-  font-size: small;
+  font-size: 18px;
 }
 
 .item button {
